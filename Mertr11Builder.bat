1bc534114faccfb861a4bf69d967c3243cb22a3a
Xbodw <120997778+Xbodw@users.noreply.github.com>
2023-03-17 18:24:58 +0800

    1

--- Mertr11Builder.bat
+++ Mertr11Builder.bat
@@ -7,14 +7,14 @@ setlocal EnableExtensions EnableDelayedExpansion
 attrib -s -h -r %cd%
 cd /d %~dp0
 FOR /F "tokens=2 delims==" %%a IN ('wmic os get OSLanguage /Value') DO set lg=%%a
-if %lg% equ 2052 (title Mertr 11ÏµÍ³¾«¼ò¾µÏñÖÆ×÷Æ÷) else (title Mertr 11 System streamlining Image Marker)
-if %lg% equ 2052 (echo Mertr 11 & echo ÏµÍ³¾«¼ò¾µÏñÖÆ×÷Æ÷) else (echo Mertr 11 & echo  System streamlining Image Marker)
+if %lg% equ 2052 (title Mertr 11ç³»ç»Ÿç²¾ç®€é•œåƒåˆ¶ä½œå™¨) else (title Mertr 11 System streamlining Image Marker)
+if %lg% equ 2052 (echo Mertr 11 & echo ç³»ç»Ÿç²¾ç®€é•œåƒåˆ¶ä½œå™¨) else (echo Mertr 11 & echo  System streamlining Image Marker)
 echo By Xbodw.
 set "mountp="
-if %lg% equ 2052 (set /p mountp=ÇëÊäÈëISO¹ÒÔØÅÌ·û:) else (set /p mountp=Press the ISO Mount Drive:)
+if %lg% equ 2052 (set /p mountp=è¯·è¾“å…¥ISOæŒ‚è½½ç›˜ç¬¦:) else (set /p mountp=Press the ISO Mount Drive:)
 if not exist %mountp% (
   if %lg% equ 2052 (
-    start /wait "" "mshta.exe" vbscript:CreateObject("Wscript.Shell"^).popup("ÎŞĞ§µÄWindows¾µÏñÇı¶¯Æ÷»òÎÄ¼ş¼Ğ: %mountp%",7,"Mertr 11ÏµÍ³¾«¼ò¾µÏñÖÆ×÷Æ÷",48^)(window.close^) 
+    start /wait "" "mshta.exe" vbscript:CreateObject("Wscript.Shell"^).popup("æ— æ•ˆçš„Windowsé•œåƒé©±åŠ¨å™¨æˆ–æ–‡ä»¶å¤¹: %mountp%",7,"Mertr 11ç³»ç»Ÿç²¾ç®€é•œåƒåˆ¶ä½œå™¨",48^)(window.close^) 
     goto pre
   ) else (
     start /wait "" "mshta.exe" vbscript:CreateObject("Wscript.Shell"^).popup("Invaild a Drive of The WindowsImage or Dirctory: %mountp%",7,"Mertr 11 System streamlining Image Marker",48^)(window.close^) 
@@ -23,7 +23,7 @@ if not exist %mountp% (
 )
 if not exist %mountp%\sources\boot.wim (
   if %lg% equ 2052 (
-    start /wait "" "mshta.exe" vbscript:CreateObject("Wscript.Shell"^).popup("ÎŞĞ§µÄWindows¾µÏñÇı¶¯Æ÷»òÎÄ¼ş¼Ğ: %mountp%,ÔÚ%mountp%\sourcesÖĞÕÒ²»µ½boot.wim",7,"Mertr 11ÏµÍ³¾«¼ò¾µÏñÖÆ×÷Æ÷",48^)(window.close^) 
+    start /wait "" "mshta.exe" vbscript:CreateObject("Wscript.Shell"^).popup("æ— æ•ˆçš„Windowsé•œåƒé©±åŠ¨å™¨æˆ–æ–‡ä»¶å¤¹: %mountp%,åœ¨%mountp%\sourcesä¸­æ‰¾ä¸åˆ°boot.wim",7,"Mertr 11ç³»ç»Ÿç²¾ç®€é•œåƒåˆ¶ä½œå™¨",48^)(window.close^) 
     goto pre
   ) else (
     start /wait "" "mshta.exe" vbscript:CreateObject("Wscript.Shell"^).popup("Invaild a Drive of The WindowsImage or Dirctory: %mountp%,Not Found boot.wim in %mountp%\sources",7,"Mertr 11 System streamlining Image Marker",48^)(window.close^) 
@@ -32,7 +32,7 @@ if not exist %mountp%\sources\boot.wim (
 )
 if not exist %mountp%\sources\install.wim (
   if %lg% equ 2052 (
-    start /wait "" "mshta.exe" vbscript:CreateObject("Wscript.Shell"^).popup("ÎŞĞ§µÄWindows¾µÏñÇı¶¯Æ÷»òÎÄ¼ş¼Ğ: %mountp%,ÔÚ%mountp%\sourcesÖĞÕÒ²»µ½install.wim,Çë²»ÒªÊ¹ÓÃesd¾µÏñ",7,"Mertr 11ÏµÍ³¾«¼ò¾µÏñÖÆ×÷Æ÷",48^)(window.close^) 
+    start /wait "" "mshta.exe" vbscript:CreateObject("Wscript.Shell"^).popup("æ— æ•ˆçš„Windowsé•œåƒé©±åŠ¨å™¨æˆ–æ–‡ä»¶å¤¹: %mountp%,åœ¨%mountp%\sourcesä¸­æ‰¾ä¸åˆ°install.wim,è¯·ä¸è¦ä½¿ç”¨esdé•œåƒ",7,"Mertr 11ç³»ç»Ÿç²¾ç®€é•œåƒåˆ¶ä½œå™¨",48^)(window.close^) 
     goto pre
   ) else (
     start /wait "" "mshta.exe" vbscript:CreateObject("Wscript.Shell"^).popup("Invaild a Drive of The WindowsImage or Dirctory: %mountp%,Not Found install.wim in %mountp%\sources,Please not use the esd Image",7,"Mertr 11 System streamlining Image Marker",48^)(window.close^) 
@@ -43,20 +43,36 @@ if not exist MakeDir md MakeDir
 if not exist Moud md Moud
 xcopy /s /i /e /y /o "%mountp%" MakeDir
 cls
-if %lg% equ 2052 (title Mertr 11ÏµÍ³¾«¼ò¾µÏñÖÆ×÷Æ÷) else (title Mertr 11 System streamlining Image Marker)
-if %lg% equ 2052 (echo Mertr 11 & echo ÏµÍ³¾«¼ò¾µÏñÖÆ×÷Æ÷) else (echo Mertr 11 & echo  System streamlining Image Marker)
+dism /Mount-Wim /WimFile:MakeDir\sources\boot.wim /index:2 /MountDir:Moud
+copy /y autounattend.xml Moud\sources\autounattend.xml
+Reg load HKLM\tSYSTEM "Moud\Windows\System32\config\System" >nul
+Reg add "HKLM\tSYSTEM\Setup\LabConfig" /v "BypassCPUCheck" /t REG_DWORD /d "1" /f >nul 2>&1
+Reg add "HKLM\tSYSTEM\Setup\LabConfig" /v "BypassRAMCheck" /t REG_DWORD /d "1" /f >nul 2>&1
+Reg add "HKLM\tSYSTEM\Setup\LabConfig" /v "BypassSecureBootCheck" /t REG_DWORD /d "1" /f >nul 2>&1
+Reg add "HKLM\tSYSTEM\Setup\LabConfig" /v "BypassStorageCheck" /t REG_DWORD /d "1" /f >nul 2>&1
+Reg add "HKLM\tSYSTEM\Setup\LabConfig" /v "BypassTPMCheck" /t REG_DWORD /d "1" /f >nul 2>&1
+Reg add "HKLM\tSYSTEM\Setup\MoSetup" /v "AllowUpgradesWithUnsupportedTPMOrCPU" /t REG_DWORD /d "1" /f >nul 2>&1
+reg unload HKLM\tSYSTEM >nul
+dism /Unmount-Wim /MountDir:Moud /commit
+cls
+if %lg% equ 2052 (title Mertr 11ç³»ç»Ÿç²¾ç®€é•œåƒåˆ¶ä½œå™¨) else (title Mertr 11 System streamlining Image Marker)
+if %lg% equ 2052 (echo Mertr 11 & echo ç³»ç»Ÿç²¾ç®€é•œåƒåˆ¶ä½œå™¨) else (echo Mertr 11 & echo  System streamlining Image Marker)
 echo By Xbodw.
 dism /Get-WimInfo /WimFile:MakeDir\sources\install.wim
 set "index=1"
-if %lg% equ 2052 (set /p index=ÇëÊäÈë¾µÏñË÷Òı:) else (set /p index=Press The Image Index:)
+if %lg% equ 2052 (set /p index=è¯·è¾“å…¥é•œåƒç´¢å¼•:) else (set /p index=Press The Image Index:)
 cls
-if %lg% equ 2052 (title Mertr 11ÏµÍ³¾«¼ò¾µÏñÖÆ×÷Æ÷) else (title Mertr 11 System streamlining Image Marker)
-if %lg% equ 2052 (echo Mertr 11 & echo ÏµÍ³¾«¼ò¾µÏñÖÆ×÷Æ÷) else (echo Mertr 11 & echo  System streamlining Image Marker)
+if %lg% equ 2052 (title Mertr 11ç³»ç»Ÿç²¾ç®€é•œåƒåˆ¶ä½œå™¨) else (title Mertr 11 System streamlining Image Marker)
+if %lg% equ 2052 (echo Mertr 11 & echo ç³»ç»Ÿç²¾ç®€é•œåƒåˆ¶ä½œå™¨) else (echo Mertr 11 & echo  System streamlining Image Marker)
 echo By Xbodw.
 dism /Mount-Wim /WimFile:MakeDir\sources\install.wim /index:%index% /MountDir:Moud
-if %lg% equ 2052 (echo ÕıÔÚĞ¶ÔØ³ÌĞò°üÓëĞŞ¸Ä×¢²á±í... ) else (echo Removeing Packages and Change Registry...)
+if %lg% equ 2052 (echo æ­£åœ¨å¸è½½ç¨‹åºåŒ…ä¸ä¿®æ”¹æ³¨å†Œè¡¨,å¯èƒ½éœ€è¦ä¸€äº›æ—¶é—´... ) else (echo Removeing Packages and Change Registry,We're GonNa take some time...)
+if %lg% equ 2052 (echo ä»¥ä¸‹æ˜¯æœªè¢«åˆ é™¤çš„ç³»ç»Ÿç¨‹åºåŒ…: ) else (echo The following packages have not been deleted:)
 for /f "tokens=2 delims=:| " %%s in ('dism /Image:Moud /Get-ProvisionedAppxPackages^|Find /i "_"') do (
-  echo %%s|Find /i "Microsoft.WindowsStore" && echo off || dism /Image:Moud /Remove-ProvisionedAppxPackage /PackageName:%%s   >nul  2>nul
+  echo %%s|Find /i "Microsoft.WindowsStore" && echo off || (
+   echo %%s|Find /i "Microsoft.DesktopAppInstaller" && echo off || dism /Image:Moud /Remove-ProvisionedAppxPackage /PackageName:%%s   >nul  2>nul
+   echo å¸è½½äº†ç¨‹åºåŒ….
+  )
 )
 rd "Moud\Program Files (x86)\Microsoft\Edge" /s /q
 rd "Moud\Program Files (x86)\Microsoft\EdgeUpdate" /s /q
@@ -69,8 +85,8 @@ Reg add "HKLM\tSYSTEM\Setup\LabConfig" /v "BypassTPMCheck" /t REG_DWORD /d "1" /
 Reg add "HKLM\tSYSTEM\Setup\MoSetup" /v "AllowUpgradesWithUnsupportedTPMOrCPU" /t REG_DWORD /d "1" /f >nul 2>&1
 reg unload HKLM\tSYSTEM >nul
 cls
-if %lg% equ 2052 (title Mertr 11ÏµÍ³¾«¼ò¾µÏñÖÆ×÷Æ÷) else (title Mertr 11 System streamlining Image Marker)
-if %lg% equ 2052 (echo Mertr 11 & echo ÏµÍ³¾«¼ò¾µÏñÖÆ×÷Æ÷) else (echo Mertr 11 & echo  System streamlining Image Marker)
+if %lg% equ 2052 (title Mertr 11ç³»ç»Ÿç²¾ç®€é•œåƒåˆ¶ä½œå™¨) else (title Mertr 11 System streamlining Image Marker)
+if %lg% equ 2052 (echo Mertr 11 & echo ç³»ç»Ÿç²¾ç®€é•œåƒåˆ¶ä½œå™¨) else (echo Mertr 11 & echo  System streamlining Image Marker)
 echo By Xbodw.
 dism /UnMount-Wim /MountDir:Moud /commit
 dism /Export-Image /SourceImageFile:MakeDir\sources\install.wim /SourceIndex:%index% /DestinationImageFile:MakeDir\sources\install-new.wim
@@ -82,7 +98,7 @@ if exist MakeDir\sources\appraiserres.dll del /f /q MakeDir\sources\appraiserres
 isomk.exe -m -o -u2 -udfver102 -bootdata:2#p0,e,bMakeDir\boot\etfsboot.com#pEF,e,bMakeDir\efi\microsoft\boot\efisys.bin MakeDir "%cd%\Mertr11.iso"
 rd /s /q MakeDir
 cls
-if %lg% equ 2052 (title Mertr 11ÏµÍ³¾«¼ò¾µÏñÖÆ×÷Æ÷) else (title Mertr 11 System streamlining Image Marker)
-if %lg% equ 2052 (echo Mertr 11 & echo ÏµÍ³¾«¼ò¾µÏñÖÆ×÷Æ÷) else (echo Mertr 11 & echo  System streamlining Image Marker)
-if %lg% equ 2052 (echo Íê³É) else (echo Complate.)
+if %lg% equ 2052 (title Mertr 11ç³»ç»Ÿç²¾ç®€é•œåƒåˆ¶ä½œå™¨) else (title Mertr 11 System streamlining Image Marker)
+if %lg% equ 2052 (echo Mertr 11 & echo ç³»ç»Ÿç²¾ç®€é•œåƒåˆ¶ä½œå™¨) else (echo Mertr 11 & echo  System streamlining Image Marker)
+if %lg% equ 2052 (echo å®Œæˆ) else (echo Complate.)
 pause
